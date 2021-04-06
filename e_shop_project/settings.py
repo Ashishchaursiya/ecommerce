74a63089201f4ebc415802ec5b0faa7ea1e72aaf
Ashishchaursiya <chaurasiyaashish383@gmail.com>
2021-04-06 09:34:10 +0530

    hg

--- e_shop_project/settings.py
+++ e_shop_project/settings.py
@@ -77,17 +77,11 @@ WSGI_APPLICATION = 'e_shop_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_USER_PASSWORD'),
-        'HOST': os.environ.get('DB_HOST'),
-        
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
-db_from_env = dj_database_url.config(conn_max_age=600)
-DATABASES['default'].update(db_from_env)
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
